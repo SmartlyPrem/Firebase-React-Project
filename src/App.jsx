4f2a55d37f24efdd98913dc16b4fbfd95b40d872
Prem Jatol <140836410+SmartlyPrem@users.noreply.github.com>
2024-02-22 17:06:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Listing from "./Listing";
 
 function App() {
   return (
-    <div className='max-w-[1200px] mx-auto grid grid-cols-5 mt-5'>
-      <Listing/>
+    <div className='max-w-[1200px] mx-auto md:grid grid-cols-5 mt-5'>
       <Form/>
+      <Listing/>
     </div>
   );
 }
